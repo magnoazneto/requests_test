e74926af63d4d7919fbf9ef4f61d5b4df1b77d6f
magnoazneto <magnoazneto@gmail.com>
2020-11-27 23:08:34 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Teste de APIS
+
+Nesse script, foi utilizado a biblioteca axios, para usar Javascript para um teste de APIS.
+Foi utilizado o método GET de 2 APIs e o método POST de uma terceira.
+
+Os testes ocorrem no arquivo resquests.js
---
+++ requests.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+
+async function makeRequests(){
+    /* 
+    Para a primeira api, utilizaremos a url api.publicapis.org/entries
+    ela traz justamente uma listagem útil para essa atividade:
+    uma listagem de APIs com os mais diversos propósitos.
+    */
+    await axios.get('https://api.publicapis.org/entries')
+    .then(function (response) {
+        // handle success
+       console.log(response.data);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+        console.log('Get method of publicapis finished.');
+    });
+
+    /* 
+    Para o segundo exemplo, podemos utilizar uma API que retorna a cotação atual do bitcoin
+    */
+
+    await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function (response) {
+    // handle success
+    console.log(response.data);
+    })
+    .catch(function (error) {
+    // handle error
+    console.log(error);
+    })
+    .then(function () {
+    // always executed
+    console.log('Get method of coindesk finished.');
+    });
+
+    /* 
+    Exemplo do método POST
+    */
+    await axios.post('https://api.html2pdf.app/v1/generate', {
+        html: '<h1>Hello world! Test for IFPI</h1>',
+        apiKey: 'cde8ef933a5ad274e4c25aacb788e3c2b9c2ccbcc67c2eaf3c911577cebe74b9'
+    })
+    .then(function (response) {
+        console.log(response.data);
+        console.log(response.status);
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    // always executed
+    console.log('Get method of html2pdf finished.');
+    });
+}
+
+
+makeRequests();
